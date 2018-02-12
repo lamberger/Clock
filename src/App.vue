65ab3858221ebbe7aa021931fa4db2ae1014b10e
Patrik Lamberger <apputvecklaren@gmail.com>
2018-02-12 22:54:03 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Clock />
+  </div>
+</template>
+
+<script>
+import Clock from './components/Clock'
+
+export default {
+  name: 'App',
+  components: {
+    Clock
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>{{ currentTime }}</h1>
+        <h2>{{ time }}</h2>
+    </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  data() {
+    return {
+      time: null,
+      currentTime: "Current time"
+    };
+  },
+  methods: {
+    display() {
+      this.time = moment().format("HH:mm:ss");
+    }
+  },
+  //Called synchronously after the instance is created.
+  created() {
+    this.time = moment().format("HH:mm:ss");
+    setInterval(() => this.display(), 1000);
+  }
+}
+</script>
